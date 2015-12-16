d49d2c9290394cd0d67f002549bc89c4555cb7d0
Max <maksymkharuk@iesebre.com>
2015-12-16 18:14:25 +0100

    Added wrong image load thread

--- app/src/main/java/com/iesebre/dam2/max/androidthreads/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/max/androidthreads/MainActivity.java
@@ -1,13 +1,61 @@
 package com.iesebre.dam2.max.androidthreads;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.ivLoadedImage);
+        findViewById(R.id.btnLoadImage).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btnLoadImage:
+                imageThread();
+                break;
+        }
+    }
+
+    private void imageThread()
+    {
+        new Thread(new Runnable() {
+            public void run() {
+                Bitmap b = loadImageFromNetwork("https://i.ytimg.com/vi/BV_d7RDYdzw/maxresdefault.jpg");
+                mImageView.setImageBitmap(b);
+            }
+        }).start();
+    }
+
+    private Bitmap loadImageFromNetwork(String url){
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+
 }
